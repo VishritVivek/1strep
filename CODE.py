4b28b84537d70f71fd4f1627a751e1e1219cfe4e
VishritVivek <vivshi654@yahoo.co.in>
2025-04-13 22:15:59 +0530

    Update CODE.py

--- CODE.py
+++ CODE.py
@@ -1,1 +1,7 @@
-hi
+print("WELCOME TO SIMPLIFIED CALCULATOR")
+a=float(input("whats your first number?"))
+b=float(input("whats your second number?"))
+print("ADDITION ",a+b)
+print("SUBTRACTION ",a-b)
+print("MULTIPLICATION ",a*b)
+print("DIVISION ",a/b)
